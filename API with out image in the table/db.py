6f82110fb3c07432cf69413a59257335ff496c34
garima-softuvo <97028428+garima-softuvo@users.noreply.github.com>
2022-01-11 10:53:11 +0530

    Add files via upload

---
+++ API with out image in the table/db.py
@@ -0,0 +1,12 @@
+
+import sqlite3
+
+conn = sqlite3.connect("dbtable.sqlite")
+
+cursor = conn.cursor()
+sql_query = """ CREATE TABLE Employees(
+    id integer PRIMARY KEY,
+    name text NOT NULL,
+    salary float NOT NULL
+)"""
+cursor.execute(sql_query)
